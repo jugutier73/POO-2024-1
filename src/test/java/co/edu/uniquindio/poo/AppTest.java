6e16dba058ef2c7a468a82b8d4acd7bfe871438d
Julián E. Gutiérre P. <jugutier@uniquindio.edu.co>
2024-02-07 17:06:11 -0500

    Pruebas

--- src/test/java/co/edu/uniquindio/poo/AppTest.java
+++ src/test/java/co/edu/uniquindio/poo/AppTest.java
@@ -1,5 +1,5 @@
 /**
- * Clase para probar el funcionamiento del código
+ * Clase para probar el funcionamiento del código de un estudiante
  * @author Área de programación UQ
  * @since 2023-08
  * 
@@ -7,6 +7,8 @@
  */
 package co.edu.uniquindio.poo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
@@ -17,13 +19,62 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
     private static final Logger LOG = Logger.getLogger(AppTest.class.getName());
 
-    /**
-     * Rigorous Test :-)
+  /**
+     * Prueba para verificar que los datos de un estudiante estén completos
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        LOG.info("Iniciado test shouldAnswerWithTrue");
-        assertTrue(true);
-        LOG.info("Finalizando test shouldAnswerWithTrue");
+    public void datosCompletos() {
+        LOG.info("Inicio datosCompletos");
+
+        Estudiante estudiante = new Estudiante("Camila", "Alzate Rios", “109453264”, "camila@uniquindio.edu.co", “315635674”, 18);
+
+        assertEquals("Camila", estudiante.nombres());
+        assertEquals("Alzate Rios", estudiante.apellidos());
+        assertEquals("109453264", estudiante.numeroIdentificacion());
+        assertEquals("camila@uniquindio.edu.co", estudiante.correo());
+        assertEquals("315635674", estudiante.telefono());
+        assertEquals(18, estudiante.edad());
+        LOG.info("Finalización datosCompletos");
     }
+
+    /**
+     * Prueba para verificar que los datos de un estudiante no estén nulos
+     */
+    @Test
+    public void datosNulos() {
+        LOG.info("Inicio datosNulos");
+        assertThrows(Throwable.class, () -> new Estudiante(null, null, null, null, null, 18));
+        LOG.info("Finalización datosNulos");
+    }
+
+    /**
+     * Prueba para verificar que los datos de un estudiante no estén vacios
+     */
+    @Test
+    public void datosVacios() {
+        LOG.info("Inicio datosVacios");
+        assertThrows(Throwable.class, () -> new Estudiante("", "", "", "", "", 0));
+        LOG.info("Finalización datosVacios");
+    }
+
+    /**
+     * Prueba para verificar que el correo del estudiante este correcto
+     */
+    @Test
+    public void edadNegativa() {
+        LOG.info("Inicio datosNegativos");
+        Estudiante estudiante = new Estudiante("Camila", "Alzate Rios", "109453264", "camila@uniquindio.edu.co", "315635674", -18);
+        LOG.info("Finalización datosNegativos");
+    }
+
+    /**
+     * Prueba para verificar que el correo del estudiante esté correcto
+     */
+    @Test
+    public void correoInvalido() {
+        LOG.info("Inicio correoInvalido");
+        Estudiante estudiante = new Estudiante("Camila", "Alzate Rios", "109453264", "cami", "315635674", 18);
+        LOG.info("Finalización correoInvalido");
+    }
+
 }
