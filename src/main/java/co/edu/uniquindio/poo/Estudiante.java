7f23064186bee85c54419843bad9a8f50d5c2a55
Julián E. Gutiérrez P. <jugutier@uniquindio.edu.co>
2024-02-14 16:18:19 -0500

    Se actualiza la prueba de datos vacíos.

--- src/main/java/co/edu/uniquindio/poo/Estudiante.java
+++ src/main/java/co/edu/uniquindio/poo/Estudiante.java
@@ -13,12 +13,12 @@ package co.edu.uniquindio.poo;
 public record Estudiante(String nombres, String apellidos, String numeroIdentificacion, String correo, String telefono, int edad) {
 
     public Estudiante {
-        assert nombres != null;
-        assert apellidos != null;
-        assert numeroIdentificacion != null;
-        assert correo != null && correo.contains("@");
-        assert telefono != null;
-        assert edad > 0;
+        assert nombres != null && !nombres.isBlank(): "El nombre debe ser diferente de null";
+        assert apellidos != null && !apellidos.isBlank(): "El apellido debe ser diferente de null";
+        assert numeroIdentificacion != null && !numeroIdentificacion.isBlank(): "El número de identificación debe ser diferente de null";
+        assert correo != null && correo.contains("@") : "El correo electrónico debe contener el símbolo @";
+        assert telefono != null : "El teléfono no puede ser null";
+        assert edad > 0 : "La edad debe ser mayor a cero";
     }
 
 }
