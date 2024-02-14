d40eb6e191aeaab3e137333e7a9c71b337ccc47e
Julián E. Gutiérrez P. <jugutier@uniquindio.edu.co>
2024-02-14 16:09:24 -0500

    Cambios en la prueba de campos vacíos

--- src/main/java/co/edu/uniquindio/poo/Estudiante.java
+++ src/main/java/co/edu/uniquindio/poo/Estudiante.java
@@ -14,12 +14,12 @@ public record Estudiante(String nombres, String apellidos, String numeroIdentifi
         int edad) {
 
     public Estudiante {
-        assert nombres != null : "El nombre debe ser diferente de null";
-        assert apellidos != null : "El apellido debe ser diferente de null";
-        assert numeroIdentificacion != null : "El número de identificación debe ser diferente de null";
+        assert nombres != null && !nombres.isBlank() : "El nombre debe ser diferente de null";
+        assert apellidos != null && !apellidos.isBlank() : "El apellido debe ser diferente de null";
+        assert numeroIdentificacion != null && !numeroIdentificacion.isBlank() : "El número de identificación debe ser diferente de null";
         assert correo != null && correo.contains("@") : "El correo electrónico debe contener el símbolo @";
         assert telefono != null : "El teléfono no puede ser null";
         assert edad > 0 : "La edad debe ser mayor a cero";
     }
- 
+
 }
