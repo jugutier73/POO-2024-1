27580399857c0326794a12876f6704b3f2cb98c2
Julián E. Gutiérrez P. <jugutier@uniquindio.edu.co>
2024-02-19 15:31:22 -0500

    Se ajusta el Javadoc

--- src/test/java/co/edu/uniquindio/poo/EstudianteTest.java
+++ src/test/java/co/edu/uniquindio/poo/EstudianteTest.java
@@ -1,38 +1,41 @@
-/**
- * Clase para probar el funcionamiento del código de un estudiante
- * @author Área de programación UQ
- * @since 2024-01
- * 
- * Licencia GNU/GPL V3.0 (https://raw.githubusercontent.com/grid-uq/poo/main/LICENSE) 
- */
+
 package co.edu.uniquindio.poo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.logging.Logger;
+
 import org.junit.jupiter.api.Test;
 
 /**
- * Unit test for simple App.
+ * Clase para probar el funcionamiento del código de un estudiante
+ * 
+ * @author Área de programación UQ
+ * @since 2024-01
+ * 
+ *        Licencia GNU/GPL V3.0
+ *        (https://raw.githubusercontent.com/grid-uq/poo/main/LICENSE)
  */
 public class EstudianteTest {
     private static final Logger LOG = Logger.getLogger(EstudianteTest.class.getName());
 
-  /**
+    /**
      * Prueba para verificar que los datos de un estudiante estén completos
      */
     @Test
     public void datosCompletos() {
         LOG.info("Inicio datosCompletos");
 
-        Estudiante estudiante = new Estudiante("Camila", "Alzate Rios", "109453264", "camila@uniquindio.edu.co", "315635674", (byte)18);
+        Estudiante estudiante = new Estudiante("Camila", "Alzate Rios", "109453264", "camila@uniquindio.edu.co",
+                "315635674", (byte) 18);
 
         assertEquals("Camila", estudiante.nombres());
         assertEquals("Alzate Rios", estudiante.apellidos());
         assertEquals("109453264", estudiante.numeroIdentificacion());
         assertEquals("camila@uniquindio.edu.co", estudiante.correo());
         assertEquals("315635674", estudiante.telefono());
-        assertEquals((byte)18, estudiante.edad());
+        assertEquals((byte) 18, estudiante.edad());
         LOG.info("Finalización datosCompletos");
     }
 
@@ -42,7 +45,7 @@ public class EstudianteTest {
     @Test
     public void datosNulos() {
         LOG.info("Inicio datosNulos");
-        assertThrows(Throwable.class, () -> new Estudiante(null, null, null, null, null, (byte)18));
+        assertThrows(Throwable.class, () -> new Estudiante(null, null, null, null, null, (byte) 18));
         LOG.info("Finalización datosNulos");
     }
 
@@ -52,7 +55,7 @@ public class EstudianteTest {
     @Test
     public void datosVacios() {
         LOG.info("Inicio datosVacios");
-        assertThrows(Throwable.class, () -> new Estudiante("", "", "", "camila@uniquindio.edu.co", "", (byte)18));
+        assertThrows(Throwable.class, () -> new Estudiante("", "", "", "camila@uniquindio.edu.co", "", (byte) 18));
         LOG.info("Finalización datosVacios");
     }
 
@@ -62,7 +65,8 @@ public class EstudianteTest {
     @Test
     public void edadNegativa() {
         LOG.info("Inicio datosNegativos");
-        assertThrows(Throwable.class, () -> new Estudiante("Camila", "Alzate Rios", "109453264", "camila@uniquindio.edu.co", "315635674", (byte)-18));
+        assertThrows(Throwable.class, () -> new Estudiante("Camila", "Alzate Rios", "109453264",
+                "camila@uniquindio.edu.co", "315635674", (byte) -18));
         LOG.info("Finalización datosNegativos");
     }
 
@@ -72,7 +76,8 @@ public class EstudianteTest {
     @Test
     public void correoInvalido() {
         LOG.info("Inicio correoInvalido");
-        assertThrows(Throwable.class, () -> new Estudiante("Camila", "Alzate Rios", "109453264", "cami", "315635674", (byte)18));
+        assertThrows(Throwable.class,
+                () -> new Estudiante("Camila", "Alzate Rios", "109453264", "cami", "315635674", (byte) 18));
         LOG.info("Finalización correoInvalido");
     }
 
